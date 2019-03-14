9216924bfa6ce7132a5d2297128a6d11281dab4f
Juan Mendoza <mendoza.jm@pucp.pe>
2019-03-14 09:42:24 -0500

    changes on teampage

--- src/Teampage/Teampage.js
+++ src/Teampage/Teampage.js
@@ -34,76 +34,107 @@ class Teampage extends Component {
         
         <Container>
           <Row className="contacttitletext"> 
-      
             <Col xs="12">
               <p>Soluciones tecnologícas diversas para un mundo cada vez más conectado</p>
             </Col>
-         
           </Row>
 
           <Row className="contacttext"> 
+            <Col xs={this.state.numerotamaño} className="teamvisiondiv">
+                <img className="teampagemainimg"
+                alt="" 
+                src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
+                </img>
+            </Col>
             
+            <Col xs={this.state.numerotamaño} className="teamvisiondiv">
+            <p className="teammainletters">Misión:</p>
+            <p>PaperCube tiene como misión ofrecer a sus clientes un abanico de soluciones tecnológicas en distintas ramas de la ingeniería que sean innovadoras, idóneas, eficientes y acordes a las nuevas tendencias, que tengan un impacto positivo y puedan incrementar el valor de su actividad productiva.</p>
+            </Col>
 
-            <Col xs={this.state.numerotamaño}>
-        
+            <Col xs={this.state.numerotamaño} className="teammissiondiv">
+            <p className="teammainletters">Visión:</p>
+            <p>PaperCube busca ser una empresa líder en el mercado de consultoría tecnológica, que sea aliado estratégico y soporte tecnológico de sus clientes. Asimismo, buscamos ser referentes en el desarrollo de soluciones innovadoras que nos permitan compartir nuestro conocimiento con nuestros clientes y con la comunidad. </p>
+            </Col>
+
+            <Col xs={this.state.numerotamaño} className="teammissiondiv">
                 <img className="teampagemainimg"
                 alt="" 
-                src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
+                src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE2IOGd?ver=2654&q=90&h=675&w=830&b=%23FFFFFFFF&aim=true"> 
                 </img>
             </Col>
-            
-            <Col xs={this.state.numerotamaño}>
-            <p>Mision:</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </Col>
-            <Col xs={this.state.numerotamaño}>
-            <p>Vision:</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            
-            </Col>
-            <Col xs={this.state.numerotamaño}>
-                <img className="teampagemainimg"
-                alt="" 
-                src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
-                </img>
-            </Col>
+
           </Row>
             <Row>
-            <Col xs="4">
-                <img className="teampagemainimg"
-                alt="" 
-                src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
-                </img>
 
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            
-            </Col>
-            <Col xs="4">
-                <img className="teampagemainimg"
-                alt="" 
-                src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
-                </img>
+                <Col xs="4">
+                    <img className="teampagemainimg"
+                    alt="" 
+                    src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
+                    </img>
+                    <p className="teamcharactermainletters">Multidisciplinariedad</p>
+                    <p className="teamcharacterletters">Nuestro equipo cuenta con profesionales de diversas ramas de ingeniería que le garantizan una solución integral.</p>
+                </Col>
 
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            
-            </Col>
-            <Col xs="4">
-                <img className="teampagemainimg"
-                alt="" 
-                src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
-                </img>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </Col>
+                <Col xs="4">
+                    <img className="teampagemainimg"
+                    alt="" 
+                    src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
+                    </img>
+                    <p className="teamcharactermainletters">Trabajo en equipo</p>
+                    <p className="teamcharacterletters">Nuestra calidad de trabajo, cooperación y cohesión nos permiten ofrecerle una solución óptima en el tiempo adecuado.</p>
+                </Col>
+
+                <Col xs="4">
+                    <img className="teampagemainimg"
+                    alt="" 
+                    src="https://www.colcircuitos.com/wp-content/uploads/2017/08/dise%C3%B1oele-03.png"> 
+                    </img>
+                    <p className="teamcharactermainletters">Tecnología</p>
+                    <p className="teamcharacterletters">Buscamos incorporar la tecnología adecuada a la solución que usted requiere. Continuamente nos capacitamos para ofrecerle lo mejor</p>
+                </Col>
+
             </Row>
+            
+            <Row className="contacttitletext"> 
+            <Col xs="12">
+              <p>Nuestro equipo:</p>
+            </Col>
+          </Row>
 
             <Row>
                 <Col xs='4' className="letrasmainphotos">
                 <p></p>
                 <img alt=""  src="https://lugachhome.files.wordpress.com/2019/02/blackie.jpeg" 
-               
-               className="teamretraits">
+                className="teamretraits">
                 </img>
-                <p>Hugo</p>
+                <p className="teamportraitletters">Hugo Deudor</p>
+                <p className="teamportraitletters">CEO</p>
+                </Col>
+
+                <Col xs='4' className="letrasmainphotos">
+                <p></p>
+                <img alt="" src="https://lugachhome.files.wordpress.com/2019/02/sachi.jpeg" 
+                   className="teamretraits">
+                </img>
+                <p className="teamportraitletters">Paco</p>
+                </Col>
+
+                <Col xs='4'className="letrasmainphotos">
+                <p></p>
+                <img alt=""  src="https://lugachhome.files.wordpress.com/2019/02/koko.jpg" 
+                className="teamretraits">
+                </img>
+                <p className="teamportraitletters">Luis</p>
+                </Col>
+
+
+                <Col xs='4' className="letrasmainphotos">
+                <p></p>
+                <img alt=""  src="https://lugachhome.files.wordpress.com/2019/02/blackie.jpeg" 
+                    className="teamretraits">
+                </img>
+                <p className="teamportraitletters">Hugo</p>
                
                 </Col>
 
@@ -113,7 +144,7 @@ class Teampage extends Component {
                
                className="teamretraits">
                 </img>
-                <p>Paco</p>
+                <p className="teamportraitletters">Paco</p>
                 
                 </Col>
 
@@ -123,10 +154,9 @@ class Teampage extends Component {
                  
                 className="teamretraits">
                 </img>
-                <p>Luis</p>
+                <p className="teamportraitletters">Luis</p>
                 
                 </Col>
-
                 </Row>
        </Container>
 
